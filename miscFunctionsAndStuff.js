62cd62ec9f69712e4762c67d983174cd2a73484e
destroyer1951 <122502793+destroyer1951@users.noreply.github.com>
2023-11-09 14:05:06 -0700

    Create miscFunctionsAndStuff.js
    Lots of misc code not all made by me but i forgor who made the rest

---
+++ miscFunctionsAndStuff.js
@@ -0,0 +1,19 @@
+// random stuff here credit to some people for making some of it lol
+
+// check for player's gamemode, param is player object and gamemode string
+function isPlayerInGameMode(player, gamemode) {
+    return world.getPlayers({ name: player.name, gameMode: gameMode }).length > 0;
+}
+
+// check for ench, returns level of ench (0 if none)
+const item = data.player.getComponent('inventory').container.getItem(data.player.selectedSlot)
+        const fortuneLevel = item.getComponent('enchantments').enchantments.hasEnchantment('fortune')
+
+// get player spawnpoint coords and the dimension of it
+player.getSpawnPoint().x
+player.getSpawnPoint().y
+player.getSpawnPoint().z
+player.getSpawnPoint().dimension
+
+// give a player an item with ItemStack class imported from minecraft/server
+data.player.getComponent('minecraft:inventory').container.addItem(new ItemStack('iron_ingot', 1))
