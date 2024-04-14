f1b40ef2dda7c20433ca572dbc101438482a868a
destroyer1951 <122502793+destroyer1951@users.noreply.github.com>
2024-04-14 13:25:07 -0600

    Update scoresEditor.js

--- scoresEditor.js
+++ scoresEditor.js
@@ -1,3 +1,5 @@
+
+// Get scores
 function getScore(target, objective) {
     try {
         if (world.scoreboard.getObjective(objective).getScore(typeof target === 'string' ? target : target.scoreboardIdentity) === undefined) {
@@ -8,9 +10,17 @@ function getScore(target, objective) {
     }
 }
 
+
+// Set scores
 function setScore(target, objective, amount, add = false) {
     const scoreObj = world.scoreboard.getObjective(objective)
     const score = (add ? scoreObj?.getScore(target) ?? 0 : 0) + amount
     scoreObj?.setScore(target, score)
     return score;
 }
+
+// Test for scores without a function using a player object (NOT RECOMMENDED!!!)
+world.scoreboard.getObjective("objective").getScore(player.scoreboardIdentity)
+
+// Add a scoreboard objective
+world.scoreboard.addObjective("objectiveName", "displayName")
