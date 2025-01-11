bd738576f61209bb35fae85a1124d324bb3e9579
destroyer1951 <122502793+destroyer1951@users.noreply.github.com>
2025-01-10 17:23:57 -0700

    Update DynamicPropertyPlayerName.js
    added tolowercase cause name case sensitivity isnt needed

--- DynamicPropertyPlayerName.js
+++ DynamicPropertyPlayerName.js
@@ -1,11 +1,11 @@
 // Only use this if you plan on accessing player's dynamic properties via their username. Usernames can change so this system isn't perfect.
 
 function getPlayerDynamicProperty(player, objective) {
-    return world.getDynamicProperty(`${player.name}:${objective}`)
+    return world.getDynamicProperty(`${player.name.toLowerCase()}:${objective}`)
 }
 
 function setPlayerDynamicProperty(player, objective, value, add = false) {
-    add && typeof value === 'number' ? world.setDynamicProperty(`${player.name}:${objective}`,  world.getDynamicProperty(`${player.name}:${objective}`) + value) : world.setDynamicProperty(`${player.name}:${objective}`, value)
+    add && typeof value === 'number' ? world.setDynamicProperty(`${player.name.toLowerCase()}:${objective}`,  world.getDynamicProperty(`${player.name.toLowerCase()}:${objective}`) + value) : world.setDynamicProperty(`${player.name.toLowerCase()}:${objective}`, value)
 }
 
 
